015f563a156338011cde292c370354201276239f
LightV <2150436066@qq.com>
2021-12-10 04:59:24 +0800

    将 svg 代码整理到 icon.vue

---
+++ src/icon.vue
@@ -0,0 +1,13 @@
+<<template>
+  <svg class="l-icon">
+    <use :xlink:href="`#i-${name}`"></use>
+  </svg>
+</template>
+<script>
+export default {
+  props: ['name']
+}
+</script>
+<style lang="scss">
+.l-icon {width: 1em; height: 1em; }
+</style>
