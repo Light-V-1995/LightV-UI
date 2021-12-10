3d0990d4f00af14f782f2e4dad6cd50c54ee0fda
LightV <2150436066@qq.com>
2021-12-11 02:32:58 +0800

    row 和 col 的雏形

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from "./button";
 import Icon from "./icon";
 import ButtonGroup from './button-group'
 import Input from './input'
+import Row from './row'
+import Col from './col'
 
 Vue.component('l-button',Button)
 Vue.component('l-icon', Icon)
 Vue.component('l-button-group', ButtonGroup)
 Vue.component('l-input', Input)
+Vue.component('l-row', Row)
+Vue.component('l-col', Col)
 
 new Vue({
     el:'#app',
