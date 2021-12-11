50ac68d511ae5d5737ec0f34014506418a25069b
LightV <2150436066@qq.com>
2021-12-12 01:22:33 +0800

    优化 toast 三种动画

--- src/toast.vue
+++ src/toast.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="toast" ref="wrapper" :class="toastClasses">
-    <div class="message">
-      <slot v-if="!enableHtml"></slot>
-      <div v-else v-html="$slots.default[0]"></div>
+  <div class="wrapper" :class="toastClasses">
+    <div class="toast" ref="toast">
+      <div class="message">
+        <slot v-if="!enableHtml"></slot>
+        <div v-else v-html="$slots.default[0]"></div>
+      </div>
+      <div class="line" ref="line"></div>
+      <span class="close" v-if="closeButton" @click="onClickClose">
+        {{closeButton.text}}
+      </span>
     </div>
-    <div class="line" ref="line"></div>
-    <span class="close" v-if="closeButton" @click="onClickClose">
-      {{closeButton.text}}
-    </span>
   </div>
 </template>
 <script>
@@ -24,7 +26,7 @@ export default {
     },
     closeButton: {
       type: Object,
-      default () {
+      default() {
         return {
           text: '关闭', callback: undefined
         }
@@ -37,47 +39,45 @@ export default {
     position: {
       type: String,
       default: 'top',
-      validator (value) {
+      validator(value) {
         return ['top', 'bottom', 'middle'].indexOf(value) >= 0
       }
     }
   },
   computed: {
-    toastClasses () {
+    toastClasses() {
       return {
         [`position-${this.position}`]: true
       }
     }
   },
-  created () {
-  },
-  mounted () {
+  mounted() {
     this.updateStyles()
     this.execAutoClose()
   },
   methods: {
-    updateStyles () {
+    updateStyles() {
       this.$nextTick(() => {
         this.$refs.line.style.height =
-            `${this.$refs.wrapper.getBoundingClientRect().height}px`
+            `${this.$refs.toast.getBoundingClientRect().height}px`
       })
     },
-    execAutoClose () {
+    execAutoClose() {
       if (this.autoClose) {
         setTimeout(() => {
           this.close()
         }, this.autoCloseDelay * 1000)
       }
     },
-    close () {
+    close() {
       this.$el.remove()
       this.$emit('close')
       this.$destroy()
     },
-    log () {
+    log() {
       console.log('测试')
     },
-    onClickClose () {
+    onClickClose() {
       this.close()
       if (this.closeButton && typeof this.closeButton.callback === 'function') {
         this.closeButton.callback(this)//this === toast实例
@@ -90,40 +90,100 @@ export default {
 $font-size: 14px;
 $toast-min-height: 40px;
 $toast-bg: rgba(0, 0, 0, 0.75);
-@keyframes fade-in {
-  0% {opacity: 0; transform: translateY(100%);}
-  100% {opacity: 1;transform: translateY(0%);}
+@keyframes slide-up {
+  0% {
+    opacity: 0;
+    transform: translateY(100%);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0%);
+  }
 }
-.toast {
-  animation: fade-in 1s;
-  font-size: $font-size; min-height: $toast-min-height; line-height: 1.8;
-  position: fixed; display: flex;
-  color: white; align-items: center; background: $toast-bg; border-radius: 4px;
-  box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50); padding: 0 16px;
+
+@keyframes slide-down {
+  0% {
+    opacity: 0;
+    transform: translateY(-100%);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0%);
+  }
+}
+
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+.wrapper {
+  position: fixed;
   left: 50%;
+  transform: translateX(-50%);
+  $animation-duration: 300ms;
+
+  &.position-top {
+    top: 0;
+
+    .toast {
+      border-top-left-radius: 0;
+      border-top-right-radius: 0;
+      animation: slide-down $animation-duration;
+    }
+  }
+
+  &.position-bottom {
+    bottom: 0;
+
+    .toast {
+      border-bottom-left-radius: 0;
+      border-bottom-right-radius: 0;
+      animation: slide-up $animation-duration;
+    }
+  }
+
+  &.position-middle {
+    top: 50%;
+    transform: translateX(-50%) translateY(-50%);
+
+    .toast {
+      animation: fade-in $animation-duration;
+    }
+  }
+}
+
+.toast {
+
+  font-size: $font-size;
+  min-height: $toast-min-height;
+  line-height: 1.8;
+  display: flex;
+  color: white;
+  align-items: center;
+  background: $toast-bg;
+  border-radius: 4px;
+  box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50);
+  padding: 0 16px;
+
   .message {
     padding: 8px 0;
   }
+
   .close {
     padding-left: 16px;
     flex-shrink: 0;
   }
+
   .line {
     height: 100%;
     border-left: 1px solid #666;
     margin-left: 16px;
   }
-  &.position-top{
-    top: 0;
-    transform: translateX(-50%);
-  }
-  &.position-bottom{
-    bottom: 0;
-    transform: translateX(-50%);
-  }
-  &.position-middle{
-    top: 50%;
-    transform: translate(-50%, -50%);
-  }
+
 }
 </style>
