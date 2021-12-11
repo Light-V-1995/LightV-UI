a219e63d830edb306128179f8251d45340ba5840
LightV <2150436066@qq.com>
2021-12-12 01:55:38 +0800

    增大tab item可点击区域

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -41,7 +41,17 @@ export default {
   }
 }
 </script>
-<style>
+<style lang="scss" scoped>
 .tabs-item {
+  flex-shrink: 0;
+  padding: 0 1em;
+  cursor: pointer;
+  border: 1px solid green;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  &.active {
+    background: red;
+  }
 }
 </style>
