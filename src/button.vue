510ec4120eb16c1ec1ee8adcda132a181673b81a
LightV <2150436066@qq.com>
2021-12-13 03:18:53 +0800

    终于配置好，搭建了官网 button 和 input

--- src/button.vue
+++ src/button.vue
@@ -2,7 +2,7 @@
   <button class="l-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
     <l-icon class="icon" v-if="icon && !loading" :name="icon"></l-icon>
     <l-icon class="loading icon" v-if="loading" name="loading"></l-icon>
-    <div class="content">
+    <div class="l-button-content">
       <slot></slot>
     </div>
 
@@ -12,22 +12,23 @@
 
 <script>
 import Icon from "./icon";
+
 export default {
-  name:'L-Button',
+  name: 'L-Button',
   // props: ['icon', 'iconPosition']
-  components:{
-    'l-icon':Icon
+  components: {
+    'l-icon': Icon
   },
-  props:{
-    icon:{},
+  props: {
+    icon: {},
     loading: {
       type: Boolean,
       default: false
     },
-    iconPosition:{
-      type:String,
-      default:'left',
-      validator (value) {
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator(value) {
         return value === 'left' || value === 'right'
       }
     },
@@ -37,54 +38,74 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+@import "var";
+
 @keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
+  0% {
+    transform: rotate(0deg)
+  }
+  100% {
+    transform: rotate(360deg)
+  }
 }
+
 .l-button {
-  font-size: var(--font-size);
-  height: var(--button-height);
+  font-size: $font-size;
+  height: $button-height;
   padding: 0 1em;
-  border-radius: var(--border-radius);
-  border: 1px solid var(--border-color);
-  background: var(--button-bg);
+  border-radius: $border-radius;
+  border: 1px solid $border-color;
+  background: $button-bg;
   display: inline-flex;
   justify-content: center;
   align-items: center;
   vertical-align: middle;
+
+  &:after {
+    content: '';
+    color: red;
+    display: none;
+  }
+
   &:hover {
-    border-color: var(--border-color-hover);
+    border-color: $border-color-hover;
   }
 
   &:active {
-    background-color: var(--button-active-bg);
+    border-color: $border-color;
   }
 
   &:focus {
     outline: none;
   }
 
-  > .icon {
-    order: 1;
-    margin-right:.1em
+  > .l-button-content {
+    line-height: 1em;
+    order: 2;
   }
 
-  > .content {
-    order: 2
+  > .icon {
+    margin-right: .1em;
+    order: 1;
   }
+
+  .loading {
+    animation: spin 1s infinite linear;
+  }
+
 
   &.icon-right {
     > .icon {
-      order: 2;
+      margin-right: 0;
       margin-left: .1em;
+      order: 2;
     }
 
-    > .content {
-      order: 1
+    > .l-button-content {
+      line-height: 1em;
+      order: 1;
     }
-  }
-  .loading {
-    animation: spin 2s infinite linear;
   }
 }
 
